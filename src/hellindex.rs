0cf01607dc99bd937df10e75ef33074dd6cf6d97
akashktesla <akashktesla@gmail.com>
2025-04-09 00:54:52 +0530

    :)

--- src/hellindex.rs
+++ src/hellindex.rs
@@ -1,14 +1,24 @@
 #![allow(warnings)]
 pub fn main(){
-    // let embedding = vec![0.7,0.32,0.43,0.64];
+    let embedding = vec![0.000000002,0.0032,0.00043,0.14];
+    let normalized_embedding = normalization(embedding,10);
+    println!("normalized_embedding: {:?}",normalized_embedding);
     // let chunk_number = generate_metadata(embedding,10);
     // println!("chunk_number: {:?}",chunk_number);
-    let chunk_number_1 = vec![4,6,7,3];
-    let chunk_number_2 = vec![4,5,8,3];
-    let diff = calculate_difference(chunk_number_1,chunk_number_2);
-    println!("diff: {:?}",diff);
+    // let chunk_number_1 = vec![4,6,7,3];
+    // let chunk_number_2 = vec![4,5,8,3];
+    // let diff = calculate_difference(chunk_number_1,chunk_number_2);
+    // println!("diff: {:?}",diff);
 
 }
+
+
+fn normalization(mut embedding: Vec<f32>,chunk_size:i32 ){
+    embedding.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    println!("sorted: {:?}",embedding);
+
+}
+
 
 pub fn generate_metadata(embedding: &Vec<f32>,chunk_size:&i32)->(Vec<i32>,i32){
    let chunk_number:Vec<i32> = embedding
