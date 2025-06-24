b5285e223989029e13c045004c3fb94e49a86431
akashktesla <akashktesla@gmail.com>
2025-06-25 00:01:20 +0530

    :)

--- src/hnsw.rs
+++ src/hnsw.rs
@@ -1,6 +1,7 @@
 #![allow(warnings)]
 use crate::grimoire::Embedding;
 use std::collections::HashMap;
+use rust_bert::pipelines::sentence_embeddings::{ SentenceEmbeddingsBuilder, SentenceEmbeddingsModelType, SentenceEmbeddingsModel };
 
 pub fn main(){
 
@@ -22,14 +23,49 @@ struct HnswNode{
     level:i32, //level of the node
     levels: HashMap<i32, Vec<NodeId>> // level - node_id
 }
+impl HnswNode{
+    fn new(id:NodeId,embedding:Embedding,level:i32,levels:HashMap<i32,Vec<NodeId>>)->HnswNode{
+        return HnswNode{
+            id,
+            embedding,
+            level,
+            levels
+        }
+
+    }
+
+}
 
 struct HnswEngine{
     entry_point: HnswNode, //dynamically updated
     max_level:i32, // for tracking
-    nodes: HashMap<NodeId,HnswNode> //global pool of nodes
+    embedding_model:SentenceEmbeddingsModel,
+    embedding_model_path:String,
+    nodes: HashMap<NodeId,HnswNode>, //global pool of nodes
+    current_node_id: NodeId
+                                   
 }
 
+impl HnswEngine{
+    fn new(max_level:i32,embedding_model:SentenceEmbeddingsModel,embedding_model_path:String){
+    }
+    fn load(&mut self,chunks:Vec<String>){
+        for i in chunks{
+            let embedding = self.generate_embeddings_string(&i);
+            let level = self.generate_level();
+            let node = HnswNode::new(self.current_node_id,embedding,level,HashMap::new());
+            self.nodes.insert(self.current_node_id,  node);
+        }
+    }
+    fn generate_level(&self)->i32{
+        return 0
+    }
+    fn generate_embeddings_string(&self,payload:&String)->Embedding{
+        let embedding = self.embedding_model.encode(&vec![payload]).expect("Failed to encode the string")[0].clone();
+        return Embedding::new(payload.clone(),embedding);
+      }
 
+}
 
 
 
