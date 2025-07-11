255414c04a48ef4d374371f16004d2f3292b0aae
akashktesla <akashktesla@gmail.com>
2025-07-11 23:53:23 +0530

    :)

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,15 @@ use rust_bert::pipelines::sentence_embeddings::{ SentenceEmbeddingsBuilder, Sent
 use ::grimoire::grimoire;
 use ::grimoire::hellindex;
 use ::grimoire::hnsw;
+use pdf_extract::extract_text;
 
 fn main() {
     // hellindex::main();
     // grimoire::main();
-    hnsw::main();
+    // hnsw::main();
+    let text = extract_text("../src/The_Art_Of_War.pdf").unwrap();
+    let chunk_size = 
+    println!("text: {:?}",text);
+
 }
 
