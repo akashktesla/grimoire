7320098f1f7cdae00041b132d85df1c199caf756
akashktesla <akashktesla@gmail.com>
2025-07-20 02:02:34 +0530

    :)

--- src/collections.rs
+++ src/collections.rs
@@ -1,5 +1,7 @@
 #![allow(warnings)]
 use std::collections::HashSet;
+use crate::grimoire::Embedding;
+use crate::hellindex::cosine_similarity;
 
 pub fn main(){
     let mut array = KSArray::new(3); 
@@ -67,3 +69,79 @@ impl KSNode{
     }
 }
 
+//K Diverse Array
+#[derive(Debug, Clone)]
+pub struct KDArray{
+    pub nodes:Vec<KDNode>,
+    pub core_node:KDNode,
+    pub k:usize,
+    inserted_ids: HashSet<usize>, 
+    k1:f32,
+    k2:f32,
+}
+
+impl KDArray{
+
+    pub fn new(node_id:&usize,embedding:&Embedding,k:usize,k1:f32,k2:f32)->KDArray{
+        let nodes = Vec::new();
+        let core_node = KDNode::new(*node_id, embedding.clone(), 0.);
+        return KDArray { 
+            nodes , 
+            k,
+            core_node,
+            inserted_ids:HashSet::new(),
+            k1,
+            k2
+        };
+    }
+
+    pub fn insert_node(&mut self, node_id:&usize, embedding:&Embedding){
+        println!("inserting node");
+        if self.inserted_ids.contains(node_id){
+            return;
+        }
+        //NOTE Better just change this as Neighbours structure or something bro this collection doesn't
+        //make sense anymore or mayb it does who knows
+        self.inserted_ids.insert(*node_id);
+        let core_similarity = cosine_similarity(&self.core_node.embedding.embedding,&embedding.embedding);
+        if self.nodes.len() >= self.k{ //aldready full
+            //sum of similarity
+            let mut sos = 0.;
+            for i in &self.nodes{
+                let similarity  = cosine_similarity(&embedding.embedding,&i.embedding.embedding);
+                sos += similarity;//minimize this
+            }
+            let  diversity_score = self.k1*(self.nodes.len() as f32-sos)+self.k2*core_similarity;
+            self.nodes.sort_by(|a,b|b.diversity_score.partial_cmp(&a.diversity_score).unwrap());
+            self.nodes.pop();
+        }
+        else{
+            let mut sos = 0.;
+            for i in &self.nodes{
+                let similarity  = cosine_similarity(&embedding.embedding,&i.embedding.embedding);
+                sos += similarity;//minimize this
+            }
+            let  diversity_score = self.k1*(self.nodes.len() as f32-sos)+self.k2*core_similarity;
+            self.nodes.push(KDNode::new(*node_id,embedding.clone(),diversity_score));
+        }
+        println!("finish inserting node");
+    }
+} 
+
+
+#[derive(Debug, Clone)]
+pub struct KDNode{
+    pub node_id:usize,
+    pub embedding:Embedding,
+    pub diversity_score:f32,
+}
+
+impl KDNode{
+    pub fn new(node_id:usize, embedding:Embedding,diversity_score:f32)->KDNode{
+        return KDNode{
+            node_id,
+            embedding,
+            diversity_score,
+        }
+    }
+}
--- src/hnsw.rs
+++ src/hnsw.rs
@@ -7,7 +7,7 @@ use rand::Rng;
 use std::fmt;
 use std::fmt::Debug;
 use pdf_extract::extract_text;
-use crate::collections::KSArray;
+use crate::collections::{KDArray,KSArray};
 
 pub fn main(){
 
@@ -17,13 +17,13 @@ pub fn main(){
 
     let embedding_model_path:String = "/home/akash/.models/all-MiniLM-L6-v2".to_string();
     let level_probability:f64 = 0.36; //rounded from 0,36;
-    let max_neighbours:usize = 16;
+    let max_neighbours:usize = usize::MAX;
     let eq_construction :usize = 200;
     let eq_search:usize = 70;
     let mut engine:HnswEngine = HnswEngine::new(embedding_model_path, level_probability, eq_construction, eq_search,max_neighbours); 
     engine.load(chunks);
     println!("Engine: {:#?}",engine.level_nodes);
-    let result = engine.traverse(&String::from("All warfare is based on deception"),&3);
+    let result = engine.traverse_max(&String::from("All warfare is based on deception"),&3);
     println!("Result: {:?}",result);
     // let result = engine.brute_force(String::from("All warfare is based on deception"),3);
     // println!("Result: {:?}",result);
@@ -33,11 +33,11 @@ pub fn main(){
     //NOTE
     //bro in the loop ur taking only top as primary candidate but take all 3 and push it to the
     //KSArray
-    
+
 }
 
 type NodeId = usize;
-    
+
 pub fn chunking(text: String, chunk_size: usize) -> Vec<String> {
     let words: Vec<&str> = text.split_whitespace().collect();
     let mut chunks = Vec::new();
@@ -73,12 +73,12 @@ struct HnswNode{
     id:NodeId,
     embedding: Embedding, 
     level:i64, //level of the node
-    neighbours: HashMap<i64, KSArray> // level - node_id
+    neighbours: HashMap<i64, KDArray> // level - node_id
 }
 
 
 impl HnswNode{
-    fn new(id:NodeId,embedding:Embedding,level:i64,neighbours:HashMap<i64,KSArray>)->HnswNode{
+    fn new(id:NodeId,embedding:Embedding,level:i64,neighbours:HashMap<i64,KDArray>)->HnswNode{
         return HnswNode{
             id,
             embedding,
@@ -154,21 +154,21 @@ impl HnswEngine{
             .create_model()
             .expect("couldn't create the model");
 
-        let entry_point = 0;
-        let max_level = -1;
-        return HnswEngine{
-            entry_point,
-            max_level,
-            embedding_model,
-            embedding_model_path,
-            nodes:HashMap::new(),
-            level_nodes:HashMap::new(),
-            current_node_id:0,
-            level_probability,
-            max_neighbours,
-            eq_construction,
-            eq_search,
-        }
+            let entry_point = 0;
+            let max_level = -1;
+            return HnswEngine{
+                entry_point,
+                max_level,
+                embedding_model,
+                embedding_model_path,
+                nodes:HashMap::new(),
+                level_nodes:HashMap::new(),
+                current_node_id:0,
+                level_probability,
+                max_neighbours,
+                eq_construction,
+                eq_search,
+            }
     }
 
     fn load(&mut self, chunks:Vec<String>){
@@ -195,7 +195,7 @@ impl HnswEngine{
                         self.level_nodes.insert(level,vec![current_node_id]);
                     }
                 }
-                self.update_neighbours_greedy(&current_node_id,&level);
+                self.update_neighbours_brute_force(&current_node_id,&level);
                 self.current_node_id = self.current_node_id+1;
                 level -= 1;
             }
@@ -203,131 +203,65 @@ impl HnswEngine{
     }
 
     fn update_neighbours_greedy(&mut self,node_id:&NodeId,level:&i64){
-        let node =  self.nodes.get(node_id).unwrap();
-        // println!("len: {:?}, eq_construction: {:?}",self.level_nodes.get(level).unwrap().len(),self.eq_construction);
-        if self.level_nodes.get(level).unwrap().len()>self.eq_construction{
-            let mut returns = KSArray::new(self.max_neighbours);
-            let uq_embedding = self.generate_embeddings_string(&node.embedding.text); 
-            let mut eqs = self.eq_construction;
-            let mut prime_candidate = self.nodes.get(&self.level_nodes.get(&level).unwrap()[0]).unwrap();
-            let mut highest_similarity=0.;
-            let node = self.nodes.get_mut(node_id).unwrap();
-            while eqs > 0 {
-                eqs-=1;
-                let mut vec_similarity = Vec::new();
-                match  prime_candidate.neighbours.get(&level) {
-                    Some(prime_candidate_neighbours)=>{
-                        for i in &prime_candidate_neighbours.nodes{
-                            let neighbour = self.nodes.get(&i.node_id).unwrap();
-                            let similarity  = cosine_similarity(&uq_embedding.embedding,&neighbour.embedding.embedding);
-                            vec_similarity.push((i.node_id,similarity));
+        let mut node = self.nodes.get(node_id).unwrap().clone();
+        let mut returns = KDArray::new(node_id,&node.embedding,self.max_neighbours,1.,1.);
+        let uq_embedding = self.generate_embeddings_string(&node.embedding.text); 
+        let mut eqs = self.eq_construction;
+        //primary candidate first node from level_nodes
+        let mut prime_candidate = &self.nodes.get(&self.level_nodes.get(&level).unwrap()[0]).unwrap().clone();
+        let mut highest_similarity=0.;
+        while eqs > 0 {
+            eqs-=1;
+            let mut vec_similarity = Vec::new();
+            match  prime_candidate.neighbours.get(&level) {
+                Some(prime_candidate_neighbours)=>{
+                    for i in &prime_candidate_neighbours.nodes{
+                        let mut neighbour = self.nodes.get(&i.node_id).unwrap().clone();
+                        let similarity  = cosine_similarity(&uq_embedding.embedding,&neighbour.embedding.embedding);
+                        vec_similarity.push((i.node_id,similarity));
 
-                            match node.neighbours.get_mut(&level){
-                                Some(neighbour) => {
-                                    neighbour.insert_node(&i.node_id,&i.similarity);
-                                }
-                                none => {
-                                    node.neighbours.insert(*level,KSArray::new(self.max_neighbours));
-                                    node.neighbours.get_mut(&level).unwrap().insert_node(&i.node_id,&i.similarity);
-                                }
-                            }
-                                
-                            match node.neighbours.get_mut(&level){
-                                Some(neighbour) => {
-                                    neighbour.insert_node(&node_id,&i.similarity);
-                                }
-                                none => {
-                                    node.neighbours.insert(*level,KSArray::new(self.max_neighbours));
-                                    node.neighbours.get_mut(&level).unwrap().insert_node(&node_id,&i.similarity);
-                                }
-                            } 
+                        //entry from node to a neighbour
+                        node.neighbours.entry(*level)
+                            .or_insert_with(|| KDArray::new(node_id,&node.embedding,self.max_neighbours,1.,1.))
+                            .insert_node(&i.node_id,&neighbour.embedding);
+
+                        //entry to neighbour also double way
+                        neighbour.neighbours.entry(*level)
+                            .or_insert_with(|| KDArray::new(node_id,&neighbour.embedding,self.max_neighbours,1.,1.))
+                            .insert_node(&node_id,&node.embedding);
                         }
-                        //sorting by similarity
-                        vec_similarity.sort_by(|a,b|b.1.partial_cmp(&a.1).unwrap());
-                        //Travel to that node
-                        if vec_similarity[0].1 > highest_similarity{
-                            prime_candidate = self.nodes.get(&vec_similarity[0].0).unwrap();
-                            highest_similarity = vec_similarity[0].1;
-                        }
-                    }
-                    None=>{
-                        eqs = 0;
+                    //sorting by similarity
+                    vec_similarity.sort_by(|a,b|b.1.partial_cmp(&a.1).unwrap());
+                    //Travel to that node
+                    if vec_similarity[0].1 > highest_similarity{
+                        prime_candidate = self.nodes.get(&vec_similarity[0].0).unwrap();
+                        highest_similarity = vec_similarity[0].1;
                     }
                 }
-            }
-
-            //greedy
-            let neighbours = self.traverse_construction(&node.embedding.text,level);
-            // println!("neighbours: {:?}",neighbours);
-            let node = self.nodes.get_mut(node_id).unwrap();
-            for i in &neighbours.nodes{
-                match node.neighbours.get_mut(&level){
-                    Some(neighbour) => {
-                        neighbour.insert_node(&i.node_id,&i.similarity);
-                    }
-                    none => {
-                        node.neighbours.insert(*level,KSArray::new(self.max_neighbours));
-                        node.neighbours.get_mut(&level).unwrap().insert_node(&i.node_id,&i.similarity);
-                    }
+                None=>{
+                    eqs = 0;
                 }
-            }
-            //updating neighbours
-            // println!("similarities_vec:{:?}",similarities_vec);
-            for i in &neighbours.nodes{ 
-                match node.neighbours.get_mut(&level){
-                    Some(neighbour) => {
-                        neighbour.insert_node(&node_id,&i.similarity);
-                    }
-                    none => {
-                        node.neighbours.insert(*level,KSArray::new(self.max_neighbours));
-                        node.neighbours.get_mut(&level).unwrap().insert_node(&node_id,&i.similarity);
-                    }
-                } 
             }
         }
+    }
 
-        else{//bruiteforce
-            let embedding = &node.embedding.clone();
-            let mut similarities_vec= Vec::new();
-            let node_list  = self.level_nodes.get(&level).unwrap();
-            //TODO: to implement a greedy approach when brute force > eq_construction
-            for i in node_list{ 
-                if node_id !=i{ //ignore calc similarity for same shit
-                    let sec_embedding = self.nodes.get(i).unwrap().embedding.embedding.clone();
-                    let similarity = cosine_similarity(&embedding.embedding, &sec_embedding);
-                    similarities_vec.push((*i,similarity));
-                }
-            }
-            //sorting by most similarity
-            similarities_vec.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
-            //inserting neighbors after sorting
-            let b = 5;
-            let max_neighbours = self.max_neighbours as usize;
-            let node = self.nodes.get_mut(node_id).unwrap();
-            for i in &similarities_vec[0..self.max_neighbours.min(similarities_vec.len())]{ 
-                match node.neighbours.get_mut(&level){
-                    Some(neighbour) => {
-                        neighbour.insert_node(&i.0,&i.1);
-                    }
-                    None => {
-                        node.neighbours.insert(*level,KSArray::new(self.max_neighbours));
-                        node.neighbours.get_mut(&level).unwrap().insert_node(&i.0,&i.1);
-                    }
-                }
-            }
-            //updating neighbours
-            // println!("similarities_vec:{:?}",similarities_vec);
-            for i in &similarities_vec{ 
-                let mut node = self.nodes.get_mut(&i.0).unwrap();
-                match node.neighbours.get_mut(&level){
-                    Some(neighbour) => {
-                        neighbour.insert_node(&node_id,&i.1);
-                    }
-                    None => {
-                        node.neighbours.insert(*level,KSArray::new(self.max_neighbours));
-                        node.neighbours.get_mut(&level).unwrap().insert_node(&node_id,&i.1);
-                    }
-                }
+    fn update_neighbours_brute_force(&mut self,node_id:&NodeId,level:&i64){
+        let mut node_clone:HnswNode = self.nodes.get(node_id).unwrap().clone();
+        let node_list  = self.level_nodes.get(&level).unwrap();
+        for i in node_list{ 
+            if node_id !=i{ //ignore calc similarity for same shit
+                let mut neighbour_clone = self.nodes.get(&i).unwrap().clone();
+                //entry from node to a neighbour
+                let mut node:&mut HnswNode =  self.nodes.get_mut(node_id).unwrap();
+                node.neighbours.entry(*level)
+                    .or_insert_with(|| KDArray::new(node_id,&node.embedding,self.max_neighbours,1.,1.))
+                    .insert_node(&i,&neighbour_clone.embedding);
+
+                let mut neighbour:&mut HnswNode =  self.nodes.get_mut(i).unwrap();
+                //entry to neighbour also double way
+                neighbour.neighbours.entry(*level)
+                    .or_insert_with(|| KDArray::new(node_id,&neighbour.embedding,self.max_neighbours,1.,1.))
+                    .insert_node(&node_id,&node_clone.embedding);
             }
         }
     }
@@ -346,6 +280,10 @@ impl HnswEngine{
 
     fn traverse(&self,user_query:&String,k:&usize)->KSArray{
         return self.traverse_core(user_query,k,&self.eq_search);
+    }
+    fn traverse_max(&self,user_query:&String,k:&usize)->KSArray{
+        let max_eq_search = usize::MAX;
+        return self.traverse_core(user_query,k,&max_eq_search);
     }
 
     fn traverse_construction(&self,user_query:&String,level:&i64,)->KSArray{
